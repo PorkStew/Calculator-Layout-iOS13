c218f032b3975dced36aa02145e40927a6399239
PorkStew <stewartclay166@gmail.com>
2022-08-23 11:53:19 -0700

    saving progress

---
+++ Calculator Layout iOS13/Model/Calculations.swift
@@ -0,0 +1,16 @@
+//
+//  Calculations.swift
+//  Calculator Layout iOS13
+//
+//  Created by Stewart Clay on 2022/08/23.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct calcualtions {
+    
+    func add(number: Int) -> Int {
+        return number
+    }
+}
--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textLabel: UILabel!
+    var number = ""
+    var total = [Int]()
+    let cal = calcualtions()
+    var result = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //gets number thats pressed
+    @IBAction func numberPressed(_ sender: UIButton) {
+        //print(sender.currentTitle!)
+        number = number + sender.currentTitle!
+        textLabel.text = number
+    }
+    //remove sender??
+    @IBAction func symbolPressed(_ sender: UIButton) {
+        var op = sender.currentTitle!
+        switch op {
+        case "+":
+            //print("+")
+            print("Calculation: \(result) + \(number)")
+            result = result + Int(number)!
+            number = ""
+            print(result)
+        case "-":
+            print("-")
+        case "*":
+            print("*")
+        case "/":
+            print("/")
+        default:
+            print("Error")
+        }
+    }
+    
+    @IBAction func equal(_ sender: Any) {
+       
+    }
+    //reset
+    @IBAction func resetButton() {
+        textLabel.text = "0"
+        number = ""
+    }
 }
 
